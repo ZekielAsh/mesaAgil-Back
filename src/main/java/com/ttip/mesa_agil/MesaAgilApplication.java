74acb68c0ebf4400f9c5778e33199fae5c37b7fd
Matias Laime <matulai1221@gmail.com>
2026-04-08 22:37:47 -0300

    Init setup

---
+++ src/main/java/com/ttip/mesa_agil/MesaAgilApplication.java
@@ -0,0 +1,13 @@
+package com.ttip.mesa_agil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesaAgilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MesaAgilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ttip/mesa_agil/MesaAgilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttip.mesa_agil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesaAgilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
