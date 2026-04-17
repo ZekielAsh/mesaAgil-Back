33692c65f9ea6a0532b28ec4a8c0a9d024eaaa79
Matias Laime <matulai1221@gmail.com>
2026-04-17 16:45:22 -0300

    feat(orders): add OrderRepository and RestaurantTableRepository

---
+++ src/main/java/com/ttip/mesa_agil/repository/OrderRepository.java
@@ -0,0 +1,8 @@
+package com.ttip.mesa_agil.repository;
+
+import com.ttip.mesa_agil.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
---
+++ src/main/java/com/ttip/mesa_agil/repository/RestaurantTableRepository.java
@@ -0,0 +1,7 @@
+package com.ttip.mesa_agil.repository;
+
+import com.ttip.mesa_agil.model.RestaurantTable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RestaurantTableRepository extends JpaRepository<RestaurantTable, Long> {
+}
