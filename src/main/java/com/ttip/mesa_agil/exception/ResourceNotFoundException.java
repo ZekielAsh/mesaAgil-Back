a038c1ca287448ee76175af28ada9641bc8fb1f1
Matias Laime <matulai1221@gmail.com>
2026-04-17 16:51:06 -0300

    feat(orders): add ResourceNotFoundException

---
+++ src/main/java/com/ttip/mesa_agil/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.ttip.mesa_agil.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
