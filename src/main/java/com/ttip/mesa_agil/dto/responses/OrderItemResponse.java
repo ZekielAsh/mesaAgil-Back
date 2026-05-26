4e1ba9e62a709b7bd4484cec8a725ee7a13eb651
Matias Laime <matulai1221@gmail.com>
2026-05-26 20:13:24 -0300

    refactor(kitchen-orders): add table number on orderItemResponse

--- src/main/java/com/ttip/mesa_agil/dto/responses/OrderItemResponse.java
+++ src/main/java/com/ttip/mesa_agil/dto/responses/OrderItemResponse.java
@@ -1,14 +1,17 @@
 package com.ttip.mesa_agil.dto.responses;
 
+import com.ttip.mesa_agil.model.enums.OrderItemStatus;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public record OrderItemResponse(
         Long id,
         Long orderId,
+        int tableNumber,
         ItemResponse item,
         int quantity,
         BigDecimal price,
-        String status,
+        OrderItemStatus status,
         LocalDateTime createdAt
 ) { }
